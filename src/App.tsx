7978a330ef16df3b57ab28eac6210ba39f2b1da2
Thomas Andersen <55490372+Thekid2002@users.noreply.github.com>
2025-09-19 09:14:13 +0200

    Here

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,20 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./home/home.tsx";
 import ConnectionInterface from "./connection-interface/connection-interface.tsx";
 
-function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
-  return (
-    <main className="container">
+export default function App() {
+    return (
+        <main>
         {ConnectionInterface()}
-        <h1>Welcome to Tauri + React</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <span>
-        <b>Hell yeah!!</b>
-      </span>
-      <span>
-        <i>is this itallic?</i>
-      </span>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
-    </main>
-  );
+        <Router>
+            <nav>
+                <Link to="/">Home</Link> | <Link to="/connect">About</Link>
+            </nav>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/connect" element={<ConnectionInterface />} />
+            </Routes>
+        </Router>
+        </main>
+    );
 }
-
-export default App;
