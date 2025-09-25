32c1087e23ae88f20ff187a168ce7ce5cf8dee70
Stoldt <lstold22@student.aau.dk>
2025-09-25 13:05:39 +0200

    Added stuffs

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Home from "./home/home.tsx";
 import ConnectionInterface from "./connection-interface/connection-interface.tsx";
 import Bridge from "./bridge/bridge.tsx";
 import ActivateMicrophone from "./activate-microphone/activate-microphone.tsx";
+import VideoRecorder from "./activate-video/VideoRecorder.tsx"
 
 function App() {
     const [_greetMsg, setGreetMsg] = useState("");
@@ -16,20 +17,19 @@ function App() {
 
     return (
         <main>
-            {ActivateMicrophone()}
-            {Bridge()}
-            {ConnectionInterface()}
             <Router>
                 <nav>
                     <Link to="/">Home</Link> | <Link to="/connect">About</Link> |{" "}
                     <Link to="/bridge">bridge</Link> |{" "}
-                    <Link to="/microphone">microphone</Link>
+                    <Link to="/microphone">microphone</Link> |{" "}
+                    <Link to="/video">video</Link>
                 </nav>
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/connect" element={<ConnectionInterface />} />
                     <Route path="/bridge" element={<Bridge />} />
                     <Route path="/microphone" element={<ActivateMicrophone />} />
+                    <Route path="/video" element={<VideoRecorder />}></Route>
                 </Routes>
             </Router>
         </main>
